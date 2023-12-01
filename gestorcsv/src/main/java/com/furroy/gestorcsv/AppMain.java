e8d9e976eea6cb220867ad8a5621afab3956e34c
Eloy Castaño <eloycastano@gmail.com>
2023-12-01 12:33:05 +0100

    Initial commit

---
+++ gestorcsv/src/main/java/com/furroy/gestorcsv/AppMain.java
@@ -0,0 +1,19 @@
+package com.furroy.gestorcsv;
+
+import com.furroy.gestorcsv.clases.CercaCSV;
+
+/**
+ *
+ * @author ELOY
+ * @version 8.0.5
+ */
+public class AppMain {
+    public static void main (String[] args){
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CercaCSV().setVisible(true);
+            }
+        });
+    }
+}
